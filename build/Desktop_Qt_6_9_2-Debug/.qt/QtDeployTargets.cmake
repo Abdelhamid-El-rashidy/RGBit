8818a3d3187a43adc907e0e1d760fab167604ab6
Abdelhamid_ElRashidy <abdelhamidofficial2005@gmail.com>
2025-10-02 23:07:16 +0300

    Remove build and IDE files from repo

--- build/Desktop_Qt_6_9_2-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_IMG-Processer-GUI_FILE /home/abdelhamid/IMG-Processer-GUI/build/Desktop_Qt_6_9_2-Debug/IMG-Processer-GUI)
-set(__QT_DEPLOY_TARGET_IMG-Processer-GUI_TYPE EXECUTABLE)
